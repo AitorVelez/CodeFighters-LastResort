a15876d8b2dd1b8742eb11e6fffec1e96c00d326
Sebi Lopez <sebi8818@gmail.com>
2018-04-05 22:50:18 +0200

    Some parametres for crater parallax

--- Last Resort Prototype/ModuleRender.cpp
+++ Last Resort Prototype/ModuleRender.cpp
@@ -49,7 +49,7 @@ update_status ModuleRender::PreUpdate()
 
 update_status ModuleRender::Update()	
 {
-	int speed = 7;
+	int speed = 35;
 
 	if(App->input->keyboard[SDL_SCANCODE_UP] == 1)
 		camera.y += speed;
