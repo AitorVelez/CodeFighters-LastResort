3e7e4e68723eda090f771d0d7d20d8636c162937
Roger Cano <theironboom@gmail.com>
2018-04-07 20:09:10 +0200

    ModuleAudio creation (in progress)

---
+++ Last Resort Prototype/ModuleAudio.cpp
@@ -0,0 +1,3 @@
+#include "Globals.h"
+#include "Application.h"
+#include "SDL_mixer\include\SDL_mixer.h"
---
+++ Last Resort Prototype/ModuleAudio.h
@@ -0,0 +1,6 @@
+#ifndef _ModuleAudio_H_
+#define _ModuleAudio_H_
+#include "Module.h"
+#include "Globals.h"
+
+#endif
