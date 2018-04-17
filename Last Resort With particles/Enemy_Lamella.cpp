837ae83957dd9f33e5218ce7d1fd5d4753e95634
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2018-04-17 16:11:37 +0200

    Starting to code Lamella (NEED SPRITES)

---
+++ Last Resort With particles/Enemy_Lamella.cpp
@@ -0,0 +1,28 @@
+#include "Application.h"
+#include "Enemy_Lamella.h"
+#include "ModuleCollision.h"
+#include "ModulePlayer.h"
+Enemy_Lamella::Enemy_Lamella(int x, int y) : Enemy(x, y)
+{
+	fly.PushBack({ 0,0,0,0 });
+
+
+
+	fly.speed = 0.05f;
+
+	animation = &fly;
+
+	collider = App->collision->AddCollider({ 0, 0, 48, 48 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
+
+	original_y = y;
+	original_x = x;
+}
+
+
+void Enemy_Lamella::Move()
+{
+	dirx = App->player->position.x - position.x; 
+	diry = App->player->position.y - position.y;
+	position.x += dirx * speed; 
+	position.y += diry * speed;
+}
