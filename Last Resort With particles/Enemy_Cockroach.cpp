2717fc505c48c63fcff484496fd06a7d572bfe3d
Gollim <virtualhowl@gmail.com>
2018-04-17 18:12:01 +0200

    Minor Changes

--- Last Resort With particles/Enemy_Cockroach.cpp
+++ Last Resort With particles/Enemy_Cockroach.cpp
@@ -6,11 +6,14 @@ Enemy_Cockroach::Enemy_Cockroach(int x, int y) : Enemy(x, y)
 {
 
 	fly.PushBack({ 9,443,32,16 });
-	fly.PushBack({ 9,459,32,15 });
 	fly.PushBack({ 9,474,32,15 });
 	fly.PushBack({ 9,490,32,15 });
-	fly.speed = 0.2f;
+	fly.PushBack({ 9,474,32,15 });
+	fly.PushBack({ 9,459,32,15 });
+
+	fly.speed = 0.1;
 	animation = &fly;
+
 
 	collider = App->collision->AddCollider({ 0, 0, 24, 24 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 
