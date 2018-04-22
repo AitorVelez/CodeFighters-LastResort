8a1ce3a97385545f8e240bef275f82dafcc72c57
Sebi Lopez <sebi8818@gmail.com>
2018-04-22 04:08:44 +0200

    Added ball p2 modules - not implemented

---
+++ Last Resort With particles/ModuleBall_P2.h
@@ -0,0 +1,110 @@
+#ifndef _MODULEBALL2_H__
+#define _MODULEBALL2_H__
+
+#include "Module.h"
+#include "Globals.h"
+#include "p2Point.h"
+#include "Animation.h"
+#include "Application.h"
+
+#define BALL_SIZE 30
+
+struct SDL_Texture;
+struct Collider;
+struct Particle;
+
+enum BALL_POSITION {
+
+	BALL_POSITION_N = 1,
+	BALL_POSITION_NNE,
+	BALL_POSITION_NE,
+	BALL_POSITION_NEE,
+	BALL_POSITION_E,
+	BALL_POSITION_SEE,
+	BALL_POSITION_SE,
+	BALL_POSITION_SSE,
+	BALL_POSITION_S,
+	BALL_POSITION_SSW,
+	BALL_POSITION_SW,
+	BALL_POSITION_SWW,
+	BALL_POSITION_W,
+	BALL_POSITION_NWW,
+	BALL_POSITION_NW,
+	BALL_POSITION_NNW,
+
+};
+
+enum BALL_AIMING {
+
+	BALL_AIMING_N = 1,
+	BALL_AIMING_NE,
+	BALL_AIMING_E,
+	BALL_AIMING_SE,
+	BALL_AIMING_S,
+	BALL_AIMING_SW,
+	BALL_AIMING_W,
+	BALL_AIMING_NW,
+
+};
+
+class ModuleBall_2 : public Module {
+
+public:
+
+	ModuleBall_2();
+	//~ModuleBall();
+
+	bool Start();
+	void Ball_Input_Movement();
+	void Ball_Input_Attack();
+	void Ball_Launch();
+	void Ball_Set_Angle();
+	void Ball_Set_Position();
+	update_status Update();
+	bool CleanUp();
+	//bool CleanUp();
+
+
+	Animation N;
+	Animation NNE;
+	Animation NE;
+	Animation NEE;
+	Animation E;
+	Animation SEE;
+	Animation SE;
+	Animation SSE;
+	Animation S;
+	Animation SSW;
+	Animation SW;
+	Animation SWW;
+	Animation W;
+	Animation NWW;
+	Animation NW;
+	Animation NNW;
+	Animation flying;
+
+	Animation* current_animation;
+	Animation test;
+	Collider* ball_collider = nullptr;
+public:
+	bool go_back = false;
+	bool in_place = false;
+	bool ball_launched = false;
+	bool ball_locked = false;
+	int angle = 0;
+	int angle_speed = 10;
+
+	float angle_aiming = 0;
+	float angle_aiming_speed = 11.25;
+
+
+	uint charge = 0;
+	iPoint ball_position;
+	iPoint center_player;
+	BALL_POSITION _ball;
+	BALL_AIMING ball_aiming;
+	SDL_Texture* texture;
+};
+
+
+#endif //_MODULEBALL_H__
