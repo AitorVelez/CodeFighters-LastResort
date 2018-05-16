e9dd2c2a5313769d82aa475733033ff07d42e97c
Aitor <aitorvelez@hotmail.com>
2018-05-16 18:27:53 +0200

    Just modules

---
+++ Last Resort_Alpha/Enemy_Barrel.h
@@ -0,0 +1,18 @@
+#ifndef __ENEMY_BARREL_H__
+#define __ENEMY_BARREL_H__
+
+#include "Enemy.h"
+
+class Enemy_Barrel : public Enemy
+{
+private:
+	Animation fly;
+	int y_original;
+public:
+
+	Enemy_Barrel(int x, int y, int HP);
+
+	void Move();
+};
+
+#endif // __ENEMY_COCKROACH_H__
