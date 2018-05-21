790d621d97dcec55420adea778839919ce9ff7c9
Aitor <aitorvelez@hotmail.com>
2018-05-21 20:09:04 +0200

    Revert "Some balance in the ball dmg"
    This reverts commit 1fd03c63908c5898a242db8d4257377ee057307c.

--- Last Resort_Alpha/ModuleBackground.cpp
+++ Last Resort_Alpha/ModuleBackground.cpp
@@ -765,7 +765,6 @@ void ModuleBackground::RenderTunnelLights()
 update_status ModuleBackground::Update()
 {
 	int scroll_speed = 1;
-	App->enemies->Balance -= 100;
 
 	// Camera Movement Conditions
 	falscamara -= scroll_speed;
--- Last Resort_Alpha/ModuleEnemies.h
+++ Last Resort_Alpha/ModuleEnemies.h
@@ -48,8 +48,8 @@ public:
 	update_status Update();
 	update_status PostUpdate();
 	bool CleanUp();
-	int Balance = 500;
 	void OnCollision(Collider* c1, Collider* c2);
+
 	bool AddEnemy(ENEMY_TYPES type, int x, int y);
 
 private:
